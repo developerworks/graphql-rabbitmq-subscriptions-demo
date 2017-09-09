cd3d6bdf73fd478f9da41a9b07c5bcfa87149f05
developerworks <developerworks@163.com>
2017-09-09 15:46:28 +0800

    move cdm logger to rabbitmq.js

--- src/pubsubs/rabbitmq.js
+++ src/pubsubs/rabbitmq.js
@@ -1,13 +1,27 @@
 import { AmqpPubSub } from 'graphql-rabbitmq-subscriptions'
 import config from '../config/index'
-import { logger } from '../utils/logger'
+import { logger } from '../config/logger'
+
+var server = require("@cdm-logger/server");
+var settings = {
+  level: "info",
+  mode: "short" // Optional: default 'short' ('short'|'long'|'dev'|'raw')
+};
+var cdmlogger = server.ConsoleLogger.create("pubsub", settings);
+cdmlogger.level('trace')
+
+
 export const pubsub = new AmqpPubSub({
   config: {
     host: config.RABBITMQ_HOST,
     port: config.RABBITMQ_PORT,
   },
   connectionListener: (err) => {
-    console.log('connect to rabbitmq', err)
+    if (err) {
+      logger.error('connect to rabbitmq error ', err)
+    } else {
+      logger.info('connected to rabbitmq')
+    }
   },
-  logger: logger
+  logger: cdmlogger
 })
