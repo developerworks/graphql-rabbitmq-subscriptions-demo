f5e2e79b1b16aeace182b315287d85da9ebaf63a
developerworks <developerworks@163.com>
2017-09-09 16:02:37 +0800

    Add user:pass for remote login

--- src/config/local.js
+++ src/config/local.js
@@ -3,7 +3,7 @@ module.exports = {
   HTTP_PORT: 8001,
   WS_HOST: '127.0.0.1',
   WS_PORT: 8003,
-  RABBITMQ_HOST: '127.0.0.1',
+  RABBITMQ_HOST: 'notification:notification@127.0.0.1',
   RABBITMQ_PORT: 5672,
   GRAPHQL_API: 'http://127.0.0.1:8001/api',
   GRAPHIQL_URL: 'ws://127.0.0.1:8003/ws',
